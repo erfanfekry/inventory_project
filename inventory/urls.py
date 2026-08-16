98c2b44888a1ff6a4cd31d87d0bc066b48d5276d
name <email>
2026-08-16 20:19:54 +0430

    add api urls

---
+++ inventory/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# urlpatterns = [
+#     path('products/', views.product_list, name='product-list'),
+#     path('products/<int:pk>/increase/', views.product_list, name='product-list'),
+#     path('products/<int:pk>/decrease/', views.product_list, name='product-list'),
+#     path('products/<int:pk>/history/', views.product_list, name='product-list'),
+# ]
